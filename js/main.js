78249fc9a39675fe87080c17131704c36a05ec0f
hjgleason <34782706+hjgleason@users.noreply.github.com>
2018-01-09 08:22:45 -0800

    updated css

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//var cardOne = "queen";
+//var cardTwo = "queen";
+//var cardThree = "king";
+//var cardFour = "king";
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log();
